ff55d7f4b8a54754f336c25fbe57e1bef11a3bd1
sprabu1989 <83269571+sprabu1989@users.noreply.github.com>
2026-02-22 00:51:09 +0530

    Add files via upload

---
+++ models/task_1_vision_transformer/train_vit.py
@@ -0,0 +1,12 @@
+
+# Training script for Vision Transformer
+
+def train_vit(model, train_loader, optimizer, criterion, device):
+    model.train()
+    for images, labels in train_loader:
+        images, labels = images.to(device), labels.to(device)
+        optimizer.zero_grad()
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
---
+++ models/task_1_vision_transformer/vit_model.py
@@ -0,0 +1,13 @@
+
+import torch
+import torch.nn as nn
+from torchvision.models import vit_b_16
+
+class VisionTransformerModel(nn.Module):
+    def __init__(self, num_classes=2):
+        super(VisionTransformerModel, self).__init__()
+        self.model = vit_b_16(pretrained=True)
+        self.model.heads.head = nn.Linear(self.model.heads.head.in_features, num_classes)
+
+    def forward(self, x):
+        return self.model(x)
